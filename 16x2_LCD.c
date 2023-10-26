fb8cd92f8f04dcf5b8617cf5dd4cc5c4b9c00db4
manigandan-d <manigandan220916@gmail.com>
2023-10-26 15:38:10 +0530

    exercises

---
+++ 16x2_LCD.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "lcd.h"
+
+static void mdelay(uint32_t count) {
+	for(uint32_t i=0; i<(count * 1000); i++);
+}
+
+int main(void) {
+	lcd_init();
+
+	lcd_print_string("Hi");
+
+	lcd_set_cursor(2, 1);
+	lcd_print_string("Hello");
+
+	mdelay(2000);
+
+	lcd_display_clear();
+	lcd_display_return_home();
+
+	while(1);
+}
